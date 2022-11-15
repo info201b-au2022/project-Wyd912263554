6333f9ce7b9286789b01d54a8bfd5a0c7bd63e91
Wyd912263554 <ywu32@uw.edu>
2022-11-14 22:58:22 -0800

    unfinished

--- source/Summary.R
+++ source/Summary.R
@@ -1,8 +1,8 @@
 library(stringr)
 library(tidyverse)
-tobacco_industry <- read_csv("/Users/wuyudong/Documents/info201/project-Wyd912263554/data/Tobacco_industry.csv")
-tobacco_use <- read_csv("/Users/wuyudong/Documents/info201/project-Wyd912263554/data/tobacco.csv")
-brain_stroke <- read_csv("/Users/wuyudong/Documents/info201/project-Wyd912263554/data/brain_stroke.csv")
+tobacco_industry <- read_csv("https://raw.githubusercontent.com/info201b-au2022/project-Wyd912263554/main/data/Tobacco_industry.csv")
+tobacco_use <- read_csv("https://raw.githubusercontent.com/info201b-au2022/project-Wyd912263554/main/data/tobacco.csv")
+brain_stroke <- read_csv("https://raw.githubusercontent.com/info201b-au2022/project-Wyd912263554/main/data/brain_stroke.csv")
 
 summary_info <- list()
 summary_info$max_country <- tobacco_industry %>%
